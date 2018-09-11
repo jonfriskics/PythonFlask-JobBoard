b359538787fce65de36dbcdce85e636ac8468905
Jon Friskics <jonfriskics@gmail.com>
2018-09-10 23:06:29 -0400

    module 6 task 7

--- jobs/app.py
+++ jobs/app.py
@@ -41,3 +41,7 @@ def jobs():
 def job(job_id):
     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', job_id, single=True)
     return render_template('job.html', job=job)
+
+@app.route('/employer/<employer_id>')
+def employer():
+    return render_template('employer.html')

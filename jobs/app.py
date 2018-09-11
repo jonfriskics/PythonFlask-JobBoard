08ac2281acdd0f6bcabb0ca736c3072d039bbf90
Jon Friskics <jonfriskics@gmail.com>
2018-09-10 23:16:41 -0400

    module 6 task 9

--- jobs/app.py
+++ jobs/app.py
@@ -45,4 +45,6 @@ def job(job_id):
 @app.route('/employer/<employer_id>')
 def employer(employer_id):
     employer = execute_sql('SELECT * FROM employer WHERE id=?', employer_id, single=True)
-    return render_template('employer.html', employer=employer)
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
+    reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
+    return render_template('employer.html', employer=employer, jobs=jobs)

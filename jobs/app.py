e3519ac69522b63f5d970a9ba074c467dbcbab07
Jon Friskics <jonfriskics@gmail.com>
2018-09-10 21:50:48 -0400

    module 3 task 4

--- jobs/app.py
+++ jobs/app.py
@@ -5,6 +5,10 @@ PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    
+
 @app.route('/')
 @app.route('/jobs')
 def jobs():

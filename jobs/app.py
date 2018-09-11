4f8d75044d9ba1be0c9504b1f67b8aaac1105b68
Jon Friskics <jonfriskics@gmail.com>
2018-09-10 22:24:53 -0400

    module 4 task 12

--- jobs/app.py
+++ jobs/app.py
@@ -34,4 +34,5 @@ def execute_sql(sql, values = (), commit = False, single = False):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-  return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)

f3135e9549021be98ffa30af1f215f86ba234a09
Jon Friskics <jonfriskics@gmail.com>
2018-09-10 21:54:03 -0400

    module 3 task 6

--- jobs/app.py
+++ jobs/app.py
@@ -9,7 +9,9 @@ def open_connection():
     connection = getattr(g, '_connection', None)
     if connection is None:
         connection = g._connection = sqlite3.connect(PATH)
-
+    connection.row_factory = sqlite3.Row
+    return connection
+    
 @app.route('/')
 @app.route('/jobs')
 def jobs():
